912c0a9cd00866d8e094d240a6be815c206192b5
Muhammad Saad <saad.salman11123@gmail.com>
2024-05-20 02:41:09 +0500

    Day 004/100
    Dived deep into cloudflare workers and Hono. Started learning AWS Deployments

--- 0to1/Week11/CodeWeek11/11.1/my-app/src/index.ts
+++ 0to1/Week11/CodeWeek11/11.1/my-app/src/index.ts
@@ -11,8 +11,33 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// };
+
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		console.log(request.body);
+// 		console.log(request.headers);
+		
+// 		if (request.method === "GET") {
+// 			return Response.json({
+// 				message: "you sent a get request"
+// 			});
+// 		} else {
+// 			return Response.json({
+// 				message: "you did not send a get request"
+// 			});
+// 		}
+// 	},
+// };
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+		return Response.json({
+			message: "Hello World!"
+		});
+	}
+}
