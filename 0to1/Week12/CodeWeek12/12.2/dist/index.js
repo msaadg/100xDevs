948181d04d02615203c5074e767a24aa14996ce7
Muhammad Saad <saad.salman11123@gmail.com>
2024-05-23 02:36:20 +0500

    Day 006/100
    Deployed simple frontend on AWS. Learnt distribution through Cloudfront CDNs. Also learnt some advance Typescript APIs

---
+++ 0to1/Week12/CodeWeek12/12.2/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// interface User {
+// 	name: string;
+// 	age: number;
+// }
+const handleEvent = (event) => {
+    console.log(`Handling event: ${event}`);
+};
+handleEvent('click'); // OK
