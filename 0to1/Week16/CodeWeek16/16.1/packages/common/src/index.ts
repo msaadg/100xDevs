122547a63e6aa4036cc5c7ff30c08eb4cb12be30
Muhammad Saad <saad.salman11123@gmail.com>
2024-07-07 01:14:47 +0500

    Day 025/100
    Learnt about turborepos/monorepos, build orchestration, and package dependencies. Bootstrapped Next.js, React, and Express in a single repo. Started learning about Auth using Cookies and NextAuth

---
+++ 0to1/Week16/CodeWeek16/16.1/packages/common/src/index.ts
@@ -0,0 +1,3 @@
+export const VALUE = "saad";
+
+export const BACKEND_URL = "https://api.google.com"
---
+++ 0to1/Week16/CodeWeek16/16.1/packages/ui/src/admin.tsx
@@ -0,0 +1,7 @@
+export const Admin = () => {
+  return (
+    <div>
+      hello from admin
+    </div>
+  )
+}
